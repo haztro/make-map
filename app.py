08a6d6ca11b403cf04877fb0c5285a626d5fc034
haztro <hfh.stitt@gmail.com>
2023-10-30 17:43:11 +1000

    test with llama.cpp,mistral

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from flask import Flask, request, jsonify, render_template
 import json
 import os
 import openai
+# from llama_cpp import Llama
 from dotenv import load_dotenv
 load_dotenv()
 openai.api_key = os.environ.get('API_KEY')
 
 app = Flask(__name__)
-
-global_graph_data = {}
+# llm = Llama(model_path="mistral-7b-instruct-v0.1.Q4_K_M.gguf")
 
 # Load prompts
 with open('prompts/main_prompt.txt', 'r') as f:
@@ -35,6 +35,14 @@ def query_gpt():
             ]
     )
     return jsonify(response.choices[0].message["content"])
+
+
+# @app.route('/expand', methods=['POST'])
+# def query_gpt():
+#     req_data = request.get_json()
+#     obj = req_data['obj']
+#     output = llm(f'[INST] {prompt.replace("<OBJECT>", obj)} [/INST]', max_tokens=256, temperature=0, stop=["<s>", "\n"], echo=True)
+#     return jsonify(output['choices'][0]['text'].split('[/INST]')[1].strip())
 
 
 if __name__ == '__main__':
